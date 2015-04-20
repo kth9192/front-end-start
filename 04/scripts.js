6f3d3a75bfb43011a3fc96086c712258b6027db7
kth9192 <kth9193@gmail.com>
2015-04-20 22:29:54 +0900

    과제 04

---
+++ 04/scripts.js
@@ -0,0 +1,30 @@
+
+var box = document.querySelectorAll('.box')[0]; //클래스가 box인 객체의 
+								//태그를 담는 배열의 [0]을 반환 
+var x = 10;
+
+function randomNumber(endNum) //매개 변수 ()에 지정된 숫자 또는 표현식의 내림값을 반환.
+//내림 값은 지정된 숫자나 표현식보다 작거나 같은 가장 가까운 정수
+{
+  return Math.floor(Math.random() * endNum) + 1;
+}
+
+function moveBox() //박스를 움직이는 함수
+{
+
+  var value = x + 5;
+
+  box.style.left = value + 'px';
+
+  x = value;
+}
+
+function startMove()
+{
+  //console.log('start')
+  window.setInterval(moveBox, 100); //지정된 시간 후 특정 스크립트 코드가 포함된
+  								//문자열을 반복하여 호출하는 메소드. 숫자는 0.001 단위이다
+}
+
+
+box.addEventListener('click', startMove); //클릭시 startMove 를 실행시킴 
